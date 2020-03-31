f03a556c60267bbc620133f516e6ef8e4f233f2c
Courtney Wagner <cwagn@me.com>
2020-03-31 09:37:23 +0800

    first commit

---
+++ coucoucourtney/courtneys-portfolio/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Heeeeyyyyy</h1>
+        <h2>whats up</h2>
+      </div>
+    );
+  }
+}
+
+export default App;

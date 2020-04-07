2dcf0ecde41462f43ff07ba57578033371926c85
Courtney Wagner <cwagn@me.com>
2020-04-07 17:49:58 +0800

    created navbar w toggle, used react bootstrap

--- courtneys-portfolio/src/index.jsx
+++ courtneys-portfolio/src/index.jsx
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 
 // import '../assets/app.css';
 import App from './components/App';
+// import 'bootstrap/dist/css/bootstrap.min.css'; <--cant use this or wont work
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
